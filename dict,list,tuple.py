32cd10d66c753239e9ae272137fec1f929c7ce97
yash kirito <yashvishwanathan@gmail.com>
2026-03-08 18:46:11 +0530

    hi

---
+++ dict,list,tuple.py
@@ -0,0 +1,12 @@
+#list 
+list1=["hi","hello this is a list "] #use square brackets and is mutable
+print(list1)
+#tuples 
+tuple1=("hi,","hello this a tuple and is immutable")
+print(tuple1)
+#set
+set1=set(["hi","hello this is a set "])
+print(set1)
+#dictionary
+dict={"hi":"hello"} 
+print(dict)
